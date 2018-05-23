8643a90f9bd064a7c62d46da7f270ed562ccd317
David Kwak <david.kwak@st.com>
2018-05-23 11:54:37 -0700

    UART race condition fix [VSCMDK-8]

--- lib/third_party/bg96/source/core/ipc/src/ipc_rxfifo.c
+++ lib/third_party/bg96/source/core/ipc/src/ipc_rxfifo.c
@@ -1,53 +1,67 @@
 /**
   ******************************************************************************
   * @file    Middlewares\ST\core\ipc\src\ipc_rxfifo.c
-  * @author  MCD Application Team
+  * @author  MCD Marketing / Vertical Application Team
   * @version V0.4
   * @date    29-June-2017
   * @brief   This file provides common code for managing IPC RX FIFO
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; COPYRIGHT 2018 STMicroelectronics</center></h2>
+  * <h2><center>&copy; Copyright © 2017 STMicroelectronics International N.V.
+  * All rights reserved.</center></h2>
   *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
+  * Redistribution and use in source and binary forms, with or without
+  * modification, are permitted, provided that the following conditions are met:
   *
-  *        http://www.st.com/software_license_agreement_liberty_v2
+  * 1. Redistribution of source code must retain the above copyright notice,
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other
+  *    contributors to this software may be used to endorse or promote products
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under
+  *    this license is void and will automatically terminate your rights under
+  *    this license.
   *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
   ******************************************************************************
-  */ 
+  */
 
 /* Includes ------------------------------------------------------------------*/
-#include "plf_config.h"
-#include "ipc_common.h"
-#include "ipc_rxfifo.h"
 #include "string.h"
+#include "ipc_rxfifo.h"
+#include "ipc_common.h"
+#include "plf_config.h"
 
-#if (USE_TRACE_INTERFACE_IPC == 1)
+#if (USE_TRACE_IPC == 1)
 #include "trace_interface.h"
-#define PrintINFO(format, args...) TracePrint(DBG_CHAN_IPC, DBL_LVL_P0, "IPC:" format "\r\n", ## args)
-#define PrintDBG(format, args...)  TracePrint(DBG_CHAN_IPC, DBL_LVL_P1, "IPC:" format "\r\n", ## args)
-#define PrintErr(format, args...)  TracePrint(DBG_CHAN_IPC, DBL_LVL_ERR, "IPC ERROR:" format "\r\n", ## args)
+#define PrintINFO(format, args...) TracePrint(DBG_CHAN_IPC, DBL_LVL_P0, "IPC:" format "\n\r", ## args)
+#define PrintDBG(format, args...)  TracePrint(DBG_CHAN_IPC, DBL_LVL_P1, "IPC:" format "\n\r", ## args)
+#define PrintErr(format, args...)  TracePrint(DBG_CHAN_IPC, DBL_LVL_ERR, "IPC ERROR:" format "\n\r", ## args)
 #define PrintBuf(pbuf, size)       TracePrintBufChar(DBG_CHAN_ATCMD, DBL_LVL_P0, (char *)pbuf, size)
 #define PrintBufHexa(pbuf, size)   TracePrintBufHex(DBG_CHAN_ATCMD, DBL_LVL_P0, (char *)pbuf, size)
 #elif (USE_PRINTF == 1)
-#define PrintINFO(format, args...)  printf("IPC:" format "\r\n", ## args)
-#define PrintDBG(format, args...)   printf("IPC:" format "\r\n", ## args)
-#define PrintErr(format, args...)   printf("IPC ERROR:" format "\r\n", ## args)
-#define PrintBuf(pbuf, size)        do {} while(0)
-#define PrintBufHexa(pbuf, size)    do {} while(0)
-#elif (USE_CONFIGPRINTF_IPC == 1)
-#define PrintINFO(format, args...)  printf("IPC:" format "\r\n", ## args)
-#define PrintDBG(format, args...)   printf("IPC:" format "\r\n", ## args)
-#define PrintErr(format, args...)   printf("IPC ERROR:" format "\r\n", ## args)
+#define PrintINFO(format, args...)  printf("IPC:" format "\n\r", ## args)
+#define PrintDBG(format, args...)   printf("IPC:" format "\n\r", ## args)
+#define PrintErr(format, args...)   printf("IPC ERROR:" format "\n\r", ## args)
 #define PrintBuf(pbuf, size)        do {} while(0)
 #define PrintBufHexa(pbuf, size)    do {} while(0)
 #else
@@ -393,11 +407,17 @@ static void RXFIFO_prepareNextMsgHeader(IPC_Handle_t *hipc)
 
 static void rearm_RX_IT(IPC_Handle_t *hipc)
 {
-    /* TODO: specific to UART... should be in ipc_uart.c */
+    HAL_StatusTypeDef err ;
+
+    /* Comment: specific to UART, should be in ipc_uart.c */
     /* rearm uart TX interrupt */
     if (hipc->Interface.interface_type == IPC_INTERFACE_UART)
     {
-        HAL_UART_Receive_IT(hipc->Interface.h_uart, (uint8_t*)g_IPC_Devices_List[hipc->Device_ID].RxChar, 1);
+        err = HAL_UART_Receive_IT(hipc->Interface.h_uart, (uint8_t*)g_IPC_Devices_List[hipc->Device_ID].RxChar, 1);
+        if(err != HAL_OK)
+        {
+           hipc->UartBusyFlag = 1U;
+        }
     }
 }
 
