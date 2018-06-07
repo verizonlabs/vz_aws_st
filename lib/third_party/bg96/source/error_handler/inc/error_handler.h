7fc11114d3a8fe664e7b7d959da404bb2a2bc595
Claude lama-Solet <claude.lama.solet@pureintegration.com>
2018-06-07 04:26:59 -0400

    [VSCMDK-46] Last batch of header changes. Verified software compiled and linked properly.

--- lib/third_party/bg96/source/error_handler/inc/error_handler.h
+++ lib/third_party/bg96/source/error_handler/inc/error_handler.h
@@ -1,35 +1,16 @@
 /**
   ******************************************************************************
-  * File Name          : error_handler.h
-  * Description        : This file contains error define utilities
+  * @file    error_handler.h
+  * @author  MCD Application Team
+  * @brief   This file contains error define utilities
   ******************************************************************************
+  * @attention
   *
-  * COPYRIGHT(c) 2017 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  * ST Confidential Information released to Verizon under NDA.
   *
   ******************************************************************************
   */
+
 #ifndef ERROR_HANDLER_H
 #define ERROR_HANDLER_H
 
@@ -65,6 +46,7 @@ typedef struct
     int             errorId; /* number identifying the error in the channel */
     error_gravity_t gravity; /* error gravity */
     uint32_t        count;   /* count how many errors have been logged since the beginning */
+
 } error_handler_decript_t;
 
 void ERROR_Handler_Init(void);
--- lib/third_party/bg96/source/error_handler/src/error_handler.c
+++ lib/third_party/bg96/source/error_handler/src/error_handler.c
@@ -1,35 +1,16 @@
 /**
   ******************************************************************************
-  * File Name          : error_handler.c
-  * Description        : This file contains error utilities
+  * @file    error_handler.c
+  * @author  MCD Application Team
+  * @brief   This file contains error utilities
   ******************************************************************************
+  * @attention
   *
-  * COPYRIGHT(c) 2017 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  * ST Confidential Information released to Verizon under NDA.
   *
   ******************************************************************************
   */
+
 #include "error_handler.h"
 #include "plf_config.h"
 #include "cmsis_os.h"
@@ -95,7 +76,6 @@ void ERROR_Handler(dbg_channels_t chan, int errorId, error_gravity_t gravity)
         NVIC_SystemReset();
         while(1);
     }
-
 
     /* increment error index */
     error_index = (error_index + 1)%  MAX_ERROR_ENTRIES;
