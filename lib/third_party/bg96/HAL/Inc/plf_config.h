1fcffea6965fed46e33b559e37db3d8b82d25cba
RameshKodam <ramesh.kodam@one.verizon.com>
2018-08-21 16:38:59 -0400

    Replacing the files as per X-CUBE-CELLULAR-v1.0.0 production release

--- lib/third_party/bg96/HAL/Inc/plf_config.h
+++ lib/third_party/bg96/HAL/Inc/plf_config.h
@@ -1,23 +1,31 @@
 /**
   ******************************************************************************
-  * @file    project_config/inc/plf_config.h
+  * @file    plf_config.h
   * @author  MCD Application Team
   * @brief   This file contains the common defines of the application
   ******************************************************************************
   * @attention
   *
-  * ST Confidential Information released to Verizon under NDA.
+  * <h2><center>&copy; Copyright (c) 2018 STMicroelectronics. 
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license 
+  * SLA0044, the "License"; You may not use this file except in compliance with 
+  * the License. You may obtain a copy of the License at:
+  *                      http://www.st.com/SLA0044
   *
   ******************************************************************************
   */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __PLF_CONFIG_H
-#define __PLF_CONFIG_H
+#ifndef PLF_CONFIG_H
+#define PLF_CONFIG_H
 
 #ifdef __cplusplus 
 extern "C" { 
 #endif 
+
+/* Includes ------------------------------------------------------------------*/
 
 /* Specific project Includes ----------------------------------------------*/
 #include "prj_config.h"
@@ -26,26 +34,10 @@ extern "C" {
 #include "plf_hw_config.h"
 #include "plf_sw_config.h"
 
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private define ------------------------------------------------------------*/
-
-/* USER CODE END Private defines */
-
-/**
-  * @}
-  */ 
-
-/**
-  * @}
-*/ 
-
 #ifdef __cplusplus 
 } 
 #endif 
 
-#endif /* __PLF_CONFIG_H */
+#endif /* PLF_CONFIG_H */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
